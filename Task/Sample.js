08ed85aaa43f984341db6283ba012b185eebe27c
Karthick <karthickms2499@gmail.com>
2023-07-06 15:33:38 +0530

    sample

---
+++ Task/Sample.js
@@ -0,0 +1,11 @@
+let ano = function() {
+    var arr=[1,2,3,4,5];
+    
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]%2!==0){
+            console.log(arr[i]);
+        }
+    }
+}
+
+ano();
