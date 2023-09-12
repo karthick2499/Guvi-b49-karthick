8e1b81d417b50515b66409ff3a88341a36350808
Karthick <karthickms2499@gmail.com>
2023-09-12 17:27:38 +0530

    React Redux Task

---
+++ Task-React/Redux-Task/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import Cart from './component/Cart.jsx'
+
+function App() {
+
+
+  return (
+    <>
+    <Cart></Cart>
+    </>
+  )
+}
+
+export default App
